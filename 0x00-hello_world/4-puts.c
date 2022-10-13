8606975cae5894706afffe4a76a78825890e91a3
Switat <dommyt97@gmail.com>
2022-10-13 10:49:29 -0700

    My commit on C language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - do not use printf
+ *
+ * Description: Print without using printf
+ * Return: print result
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
