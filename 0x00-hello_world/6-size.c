2b564027c11f60cdb0ec1373a0a11ac6a15ddd89
Switat <dommyt97@gmail.com>
2022-10-13 11:59:54 -0700

    My project commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - C language program
+ *
+ * Description: This program prints system data
+ * Return: Run the code successfully;
+ */
+
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n" sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n" sizeof(long long int));
+	printf("size of a float: %d byte(s)\n" sizeof(float));
+	return (0);
+}
