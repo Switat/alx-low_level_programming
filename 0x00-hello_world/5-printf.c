43d34950bd9469c6b362ef257db5770bcab937b2
Switat <dommyt97@gmail.com>
2022-10-13 10:11:51 -0700

    My commit on C language

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdin.h>
+
+/**
+ * main: first program in C language
+ *
+ * Description: This script prints everything properly
+ * Return: This function return the value of 0 to show sucess
+ */
+
+Int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
