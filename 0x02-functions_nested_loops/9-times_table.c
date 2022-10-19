e80014d465236edca15a445445630fedfbe9d6e2
Switat <dommyt97@gmail.com>
2022-10-19 11:56:32 -0700

    My commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * main - check code
+ * times_table: prints the 9 times table, starting with 0
+ * Return: 0
  */
 void time_table(void)
 {
