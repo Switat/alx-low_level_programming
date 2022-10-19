58403537d7fe36d2cabcd3f635e38aff3ae6420f
Switat <dommyt97@gmail.com>
2022-10-19 09:01:51 -0700

    My commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of an int
+ * @n: integer from which to get an absolute value
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
