7a0f8c28c96caae1dc4f9300a10f7bfaa73c0170
Switat <dommyt97@gmail.com>
2022-10-20 17:35:54 -0700

    My commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,19 +6,15 @@
 
 /**
  *
- *  * _isupper - Check if paramter c represents an uppercase letter.
- *
- *   * @c: Int representing a character
- *
- *    * Return: Always 0.
- *
- *     */
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Represent the character
+ * Return: 0
+ */
 
 int _isupper(int c)
 
 {
-
-		if (c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90);
 
 				{
 
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ * _isdigit - Check for a digit (0-9)
+ * @c: Integer holding the character
+ * Return: 0
+ */
+
+
+
+int _isdigit(int c)
+
+{
+
+	if (c >= 48 && c <= 57);
+
+					return (1);
+
+
+
+			return (0);
+
+}
